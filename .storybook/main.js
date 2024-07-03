17149db3a788c3d2b169e8ab5173d7d1b17e257b
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2024-07-02 20:15:56 -0400

    Scaffold initial project

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+/** @type { import('@storybook/web-components-vite').StorybookConfig } */
+const config = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: {
+    name: "@storybook/web-components-vite",
+    options: {},
+  },
+  core: {
+    disableTelemetry: true,
+  },
+};
+export default config;
---
+++ README.md
@@ -0,0 +1,3 @@
+# Playwright Visual Regression Testing
+
+The purpose of this repository is to experiment with Playwright's visual regression testing and storybook.
