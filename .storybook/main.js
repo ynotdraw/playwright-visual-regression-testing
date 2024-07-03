fe25b446fd50f5cc3f766e560e3d928dda5c5a40
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2024-07-02 20:27:48 -0400

    Add prettier

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,12 @@
 /** @type { import('@storybook/web-components-vite').StorybookConfig } */
 const config = {
   stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: {
-    name: "@storybook/web-components-vite",
+    name: '@storybook/web-components-vite',
     options: {},
   },
   core: {
--- stories/Button.js
+++ stories/Button.js
@@ -5,13 +5,25 @@ import './button.css';
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ primary, backgroundColor = null, size, label, onClick }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+export const Button = ({
+  primary,
+  backgroundColor = null,
+  size,
+  label,
+  onClick,
+}) => {
+  const mode = primary
+    ? 'storybook-button--primary'
+    : 'storybook-button--secondary';
 
   return html`
     <button
       type="button"
-      class=${['storybook-button', `storybook-button--${size || 'medium'}`, mode].join(' ')}
+      class=${[
+        'storybook-button',
+        `storybook-button--${size || 'medium'}`,
+        mode,
+      ].join(' ')}
       style=${styleMap({ backgroundColor })}
       @click=${onClick}
     >
