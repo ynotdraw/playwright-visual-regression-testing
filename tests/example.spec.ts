8cd3331ae65152b6052b1d4022c13cbe0077077d
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2024-08-24 17:48:08 -0400

    Remove old playwright example tests

--- tests/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('renders a button', async ({ page }) => {
-  await page.goto('/iframe.html?args=&id=button--primary&viewMode=story');
-
-  await page.waitForSelector('glide-core-button');
-
-  await expect(page).toHaveScreenshot(['button.test.png']);
-});
